85992749fb2f1038b1ea3fed56b650eb0b62fc52
Victor Leung <victorkleung95@gmail.com>
2021-09-22 19:46:09 -0600

    Responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ var maxSize = (size *4) + 15;
 var minSie = (size/3) - 15 + (size *2);
 
 /*button*/
+function menuToggle() {
+  var x = document.getElementByID('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
